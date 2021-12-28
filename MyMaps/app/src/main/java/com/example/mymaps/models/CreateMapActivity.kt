d52bd45c01a34e0b8c215f176f8f00d7c6257ad0
JasonRose95 <jasonrosenthalr@gmail.com>
2021-12-28 14:06:46 -0800

    My maps updated

--- MyMaps/app/src/main/java/com/example/mymaps/models/CreateMapActivity.kt
+++ MyMaps/app/src/main/java/com/example/mymaps/models/CreateMapActivity.kt
@@ -1,7 +1,14 @@
 package com.example.mymaps.models
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import com.example.mymaps.EXTRA_MAP_TITLE
 import com.example.mymaps.R
 import com.example.mymaps.databinding.ActivityCreateMapBinding
@@ -11,12 +18,16 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
 
+private const val TAG = "CreateMapActivity "
 class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityCreateMapBinding
+    private var markers: MutableList<Marker> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +41,13 @@ class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        mapFragment.view?.let {
+            Snackbar.make(it, "Long press to add a marker", Snackbar.LENGTH_INDEFINITE)
+                .setAction("OK", {})
+                .setActionTextColor(ContextCompat.getColor(this, android.R.color.white))
+                .show()
+        }
     }
 
     /**
@@ -44,9 +62,45 @@ class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        mMap.setOnInfoWindowClickListener {markerToDelete ->
+            Log.i(TAG,"onWindowClickListener- delete this marker")
+            markers.remove(markerToDelete)
+            markerToDelete.remove()
+        }
+
+        mMap.setOnMapLongClickListener { latLng ->
+            Log.i(TAG, "onMapLongClickListener")
+            showAlertDialog(latLng)
+
+        }
+
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun showAlertDialog(latLng: LatLng) {
+        val placeFormView = LayoutInflater.from(this).inflate(R.layout.dialog_create_place, null)
+        val dialog = AlertDialog.Builder(this)
+            .setTitle("Create a marker")
+            .setView(placeFormView)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("OK", null)
+            .show( )
+
+        dialog.getButton(DialogInterface.BUTTON_POSITIVE).setOnClickListener {
+            val title = placeFormView.findViewById<EditText>(R.id.etTitle).text.toString()
+            val description = placeFormView.findViewById<EditText>(R.id.etDescription).text.toString()
+            if (title.trim().isEmpty() || description.trim().isEmpty()){
+                Toast.makeText(this, "Place must have non-empty title and description", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            val marker = mMap.addMarker(
+                MarkerOptions().position(latLng).title(title).snippet(description)
+            )
+            markers.add(marker)
+            dialog.dismiss()
+        }
     }
 }
