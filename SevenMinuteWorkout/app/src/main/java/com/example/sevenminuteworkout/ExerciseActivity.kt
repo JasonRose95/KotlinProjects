a5ea9630d7e2556cc5ff3f896dfe357040b8d33b
JasonRose95 <jasonrosenthalr@gmail.com>
2022-02-07 19:17:03 -0800

    sevenminuteworkout

---
+++ SevenMinuteWorkout/app/src/main/java/com/example/sevenminuteworkout/ExerciseActivity.kt
@@ -0,0 +1,28 @@
+package com.example.sevenminuteworkout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.example.sevenminuteworkout.databinding.ActivityExerciseBinding
+
+class ExerciseActivity : AppCompatActivity() {
+    private var binding:ActivityExerciseBinding ?= null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityExerciseBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarExercise)
+
+        if (supportActionBar != null){
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
+
+
+        binding?.toolbarExercise?.setNavigationOnClickListener {
+            onBackPressed()
+        }
+
+    }
+}
--- SevenMinuteWorkout/app/src/main/java/com/example/sevenminuteworkout/MainActivity.kt
+++ SevenMinuteWorkout/app/src/main/java/com/example/sevenminuteworkout/MainActivity.kt
@@ -1,18 +1,31 @@
 package com.example.sevenminuteworkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
 import android.widget.Toast
+import com.example.sevenminuteworkout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var binding:ActivityMainBinding?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
 
-        val flStartButton: FrameLayout = findViewById(R.id.flStart)
-        flStartButton.setOnClickListener{
-            Toast.makeText(this@MainActivity,"Here we will start the excercise.",Toast.LENGTH_SHORT).show()
+//        val flStartButton: FrameLayout = findViewById(R.id.flStart)
+        binding?.flStart?.setOnClickListener {
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
         }
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        binding = null
+    }
 }
+
