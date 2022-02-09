0aad1adba844af5a03062bfc6c3d1ef0fbc0f313
JasonRose95 <jasonrosenthalr@gmail.com>
2022-02-08 16:23:04 -0800

    SevenMinuteWorkout

---
+++ SevenMinuteWorkout/app/src/main/java/com/example/sevenminuteworkout/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.sevenminuteworkout
+
+object Constants {
+
+    fun defaultExerciseList(): ArrayList<ExerciseModel>{
+        val exerciseList = ArrayList<ExerciseModel>()
+        val halos = ExerciseModel(
+
+        )
+
+        return exerciseList
+    }
+}
